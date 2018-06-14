90c3885d6f4021f33f842a9e7b4e0d70483eb47a
komuroshun <snkmr56@gmail.com>
2018-06-15 07:56:53 +0900

    Change project name

--- app/src/main/java/com/example/komuroshun/androidcardui/CardListAdapter.java
+++ app/src/main/java/com/example/komuroshun/androidcardui/CardListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.komuroshun.carduiapp;
+package com.example.komuroshun.androidcardui;
 
 import android.content.Context;
 import android.content.pm.PackageInfo;
--- app/src/main/java/com/example/komuroshun/androidcardui/CardUiListActivity.java
+++ app/src/main/java/com/example/komuroshun/androidcardui/CardUiListActivity.java
@@ -1,4 +1,4 @@
-package com.example.komuroshun.carduiapp;
+package com.example.komuroshun.androidcardui;
 
 import android.net.Uri;
 import android.os.Bundle;
